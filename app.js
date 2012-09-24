ba47b590467c1ee4af680644ca3c950d6bd9836c
Dean Sofer <proloser@hotmail.com>
2012-09-24 03:14:54 -0700

    More hacking

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-var app = angular.module('plunker', ['ui', 'ngResource']);
+var app = angular.module('api', ['ui', 'ngResource']);
 
-app.controller('MainCtrl', function($scope, $resource) {
+app.value('ui.config', {
+  codemirror: {
+    theme: 'monokai',
+    lineNumbers: true
+  }
+});
+
+app.factory('endpoints', ['$resource', function($resource){
+  var endpoints = $resource('global.json', {}, {
+    get: { isArray:true, method: 'GET' }
+  });
+  return endpoints;
+}]).factory('tokens', ['$http', function($http){
+  var data = [];
+  $http.get('tokens.json').success(function(response, code){
+    angular.forEach(response, function(value, key){
+      data.unshift({
+        key: key,
+        value: value
+      });
+    });
+  });
+  return data;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'endpoints', function($scope, endpoints) {
+  $scope.logs = [];
   $scope.filters = {};
-  var endpoints = $resource('global.json');
+
   $scope.endpoints = endpoints.get();
-  $scope.open = function(title, data) {
+  $scope.open = function(data) {
+    if (!data.method) {
+      if (data.get)
+        data.method = 'GET';
+      else if (data.post)
+        data.method = 'POST';
+    }
+    if (!data.postData && angular.isObject(data.post)) {
+      data.postData = JSON.stringify(data.post);
+    }
     $scope.data = data;
-    $scope.title = title;
   };
-}).controller('TestingCtrl', function($scope, $http) {
-  $scope.tokens = [{}];
-  $scope.addToken = function(last, token, index){
-    if (last && token.key && token.value)
-      $scope.tokens.push({});
-    if (!last && !token.key && !token.value)
-      $scope.tokens.splice(index, 1);
+}]).controller('TestingCtrl', ['$scope', '$http', 'tokens', function($scope, $http, tokens) {
+
+  $scope.tokens = tokens;
+  $scope.tokens.push({});
+
+  $scope.autoAdd = function(dataset, index){
+    var item = dataset[index],
+        last = (index === dataset.length - 1);
+    if (last && item.key)
+      dataset.push({});
+    if (!last && !item.key && !item.value)
+      dataset.splice(index, 1);
   };
+
   $scope.codeClass = function(code){
     return {
       'label-info' : code < 300,
@@ -23,19 +63,27 @@ app.controller('MainCtrl', function($scope, $resource) {
       'label-important' : code > 399
     };
   };
+
   $scope.test = function(data) {
     function done(response, code) {
-      data.response = response;
-      data.code = code;
+      data.response = options.response = response;
+      data.code = options.code = code;
+      $scope.loading = false;
     }
     var options = {
       url: data.host + data.path,
-      method: data.method || 'GET'
+      method: data.method
     };
     angular.forEach($scope.tokens, function(token, index){
-      if (index !== $scope.tokens.length - 1)
-        options.url = options.url.replace('{'+token.key+'}', token.value, 'gi');
+      if (index !== $scope.tokens.length - 1) {
+        console.log(options.url = options.url.replace('{'+token.key+'}', token.value, 'gi'));
+      }
     });
+    $scope.loading = true;
+    console.log(options);
     $http(options).success(done).error(done);
+    options.data = angular.copy(data);
+    $scope.logs.push(options);
   };
-});
+
+}]);
